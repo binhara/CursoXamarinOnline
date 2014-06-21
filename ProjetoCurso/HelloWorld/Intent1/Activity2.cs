512ea3aaf750d24c464e4902ea2fb5561aa9da09
guigomesa <guigomesa@outlook.com>
2014-06-21 11:05:10 -0300

    Aula 01 step1

---
+++ ProjetoCurso/HelloWorld/Intent1/Activity2.cs
@@ -0,0 +1,24 @@
+using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Intent1
+{
+	public class Activity2 : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+
+		}
+	}
+
+}
+
+
---
+++ ProjetoCurso/HelloWorld/Intent1/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Intent1
+{
+	[Activity (Label = "Intent1", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			
+			button.Click += delegate {
+
+				var activity = new Intent(this,typeof(Activity2));
+				activity.PutExtra("dado1","Ola intent 2");
+
+				StartActivity(activity);
+				
+			};
+		}
+	}
+}
+
+

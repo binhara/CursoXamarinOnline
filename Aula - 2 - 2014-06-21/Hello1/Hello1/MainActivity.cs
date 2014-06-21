1b2c08c120940c4c53c55f2f6060c28dba56ead7
onlinesites <onlinesites@onlinesites.com.br>
2014-06-21 10:31:10 -0300

    Aula 2

---
+++ Aula - 2 - 2014-06-21/Hello1/Hello1/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Hello1
+{
+	[Activity (Label = "Hello1", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+			var layout = new LinearLayout (this);
+			layout.Orientation = Orientation.Vertical;
+
+			var alabel = new TextView (this);
+			alabel.Text= "Olá!!";
+
+			var aButton = new Button (this);
+			aButton.Text = "Meu botão";
+
+			aButton.Click += (sender, e) => {
+				count ++;
+				alabel.Text= string.Format("Cliquei!! {0}",count);
+			};
+
+			layout.AddView (alabel);
+			layout.AddView (aButton);
+
+			SetContentView (layout);
+
+		}
+	}
+}
+
+
